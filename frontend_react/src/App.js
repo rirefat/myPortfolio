7dcf06829ff325c7474e897b084d5fa42b426020
rirefat <rirefat.official@gmail.com>
2022-08-14 03:54:30 +0600

    done folder and file structure

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="app">
+      <Navbar></Navbar>
+      <Header></Header>
+      <About></About>
+      <Work></Work>
+      <Skills></Skills>
+      <Testimonial></Testimonial>
+      <Footer></Footer>
     </div>
   );
 };
